0311c4236c53d6c5b23ba4a96eb9c98bbc847395
Rafael Niquefa <niquefa@gmail.com>
2021-05-30 17:46:06 -0500

    Change app executable and gitignore

---
+++ app.py
@@ -0,0 +1,36 @@
+import os
+import logging
+import time
+from datetime import datetime
+from flask import Flask, jsonify, request
+from analyzer import config
+from analyzer import algorithms
+from analyzer import controller
+
+app = Flask(__name__)
+
+
+@app.route("/health")
+def health_check():
+    return jsonify({"status": "the analyzer is healthy"})
+
+
+@app.route("/mutant", methods=["POST"])
+def mutant():
+    return controller.process_dna(request.get_json())
+
+
+@app.route("/stats")
+def stats():
+    return controller.get_stats()
+
+
+if __name__ == "__main__":
+
+    if "ENVIRONMENT" in os.environ.keys() and os.environ["ENVIRONMENT"] == "TEST":
+        logging.basicConfig(level=logging.INFO)
+        app.run(host="127.0.0.1", port=3000)
+    else:
+        print(f"ANALIZER RUNNING logging level = logging.DEBUG")
+        logging.basicConfig(level=logging.INFO)
+        app.run(host="0.0.0.0", port=80)
