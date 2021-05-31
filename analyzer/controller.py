2959b84faf63255607d229ff8956b9eb02bbd363
Rafael Niquefa <niquefa@gmail.com>
2021-05-30 23:11:48 -0500

    Removed automatich test sh file, add database acces error handling

--- analyzer/controller.py
+++ analyzer/controller.py
@@ -9,19 +9,31 @@ from analyzer import gateway
 
 def process_dna(json_payload):
     dna = json_payload["dna"]
+    valid_process = True
     try:
 
         if not analyzer_utils.is_valid(dna):
             jsonify({"Request error": constants.UNPROCESSABLE_ENTITY}), 422
         is_mutant = algorithms.is_mutant(dna)
-        gateway.save_detection(dna, is_mutant)
-
-        return jsonify({"is_mutant": is_mutant}), 200 if is_mutant else 403
 
     except Exception as error_instance:
+        valid_process = False
+        analyzer_utils.log_error(error_instance, constants.UNPROCESSABLE_ENTITY)
 
-        analyzer_utils.log_error(error_instance, constants.UNSUPORTED_DATA_TYPE)
-        return jsonify({"Request error": constants.UNSUPORTED_DATA_TYPE}), 415
+    if not valid_process:
+        analyzer_utils.log_error(error_instance, constants.UNSUPPORTED_DATA_TYPE)
+        return jsonify({"Unsuported data type error": constants.UNSUPPORTED_DATA_TYPE}), 415
+
+    try:
+        gateway.save_detection(dna, is_mutant)
+    except Exception as error_instance:
+        analyzer_utils.log_error(error_instance, constants.DATABASE_LAYER_ERROR)
+        return (
+            jsonify({f"is_mutant: {is_mutant}": constants.DATABASE_LAYER_ERROR}),
+            502,
+        )
+
+    return jsonify({"is_mutant": is_mutant}), 200 if is_mutant else 403
 
 
 def get_stats():
